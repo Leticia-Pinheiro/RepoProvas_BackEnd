6dd583009b4605efa5564af6de95195a0d4edb19
Leticia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-09-14 18:06:31 -0300

    Create table teachersDisciplines

---
+++ prisma/migrations/20220914210609_create_table_teachers_disciplines/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "teachersDisciplines" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "teacherId" INTEGER NOT NULL,
+    "disciplineId" INTEGER NOT NULL,
+
+    CONSTRAINT "teachersDisciplines_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teachersDisciplines_name_key" ON "teachersDisciplines"("name");
+
+-- AddForeignKey
+ALTER TABLE "teachersDisciplines" ADD CONSTRAINT "teachersDisciplines_teacherId_fkey" FOREIGN KEY ("teacherId") REFERENCES "teachers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teachersDisciplines" ADD CONSTRAINT "teachersDisciplines_disciplineId_fkey" FOREIGN KEY ("disciplineId") REFERENCES "disciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
